f4724b973d911123ec319fb900baf6546875c2db
Vishal Agrawal <vicky.baba424242@gmail.com>
2026-03-06 17:42:05 +0530

    nerr

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,7 @@
 import UIKit
 import Flutter
+import Firebase
+import GoogleSignIn
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -9,7 +11,23 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
 
+    FirebaseApp.configure()
+
     GeneratedPluginRegistrant.register(with: self)
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  override func application(
+    _ app: UIApplication,
+    open url: URL,
+    options: [UIApplication.OpenURLOptionsKey : Any] = [:]
+  ) -> Bool {
+
+    if GIDSignIn.sharedInstance.handle(url) {
+      return true
+    }
+
+    return super.application(app, open: url, options: options)
   }
 }
