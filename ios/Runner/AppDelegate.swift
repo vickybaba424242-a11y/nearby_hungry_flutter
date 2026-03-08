4a1a6595681b695288aec158f79d004687848e5f
Vishal Agrawal <vicky.baba424242@gmail.com>
2026-03-08 17:55:03 +0530

    29

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,27 +1,16 @@
 import UIKit
 import Flutter
 import GoogleSignIn
-import Firebase
-import FirebaseMessaging
-import UserNotifications
 
 @main
-@objc class AppDelegate: FlutterAppDelegate, UNUserNotificationCenterDelegate, MessagingDelegate {
+@objc class AppDelegate: FlutterAppDelegate {
 
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
 
-    FirebaseApp.configure()
-
-    UNUserNotificationCenter.current().delegate = self
-    Messaging.messaging().delegate = self
-
-    application.registerForRemoteNotifications()
-
     GeneratedPluginRegistrant.register(with: self)
-
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
@@ -31,13 +20,5 @@ import UserNotifications
     options: [UIApplication.OpenURLOptionsKey : Any] = [:]
   ) -> Bool {
     return GIDSignIn.sharedInstance.handle(url)
-  }
-
-  override func userNotificationCenter(
-    _ center: UNUserNotificationCenter,
-    willPresent notification: UNNotification,
-    withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void
-  ) {
-    completionHandler([.alert, .badge, .sound])
   }
 }
