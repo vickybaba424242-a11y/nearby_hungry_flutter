2cc4956ab259ab524c3b7176c05b645a4916c160
Vishal Agrawal <vishal.agrawal5001@gmail.com>
2026-07-26 15:28:55 +0530

    Added admin support chat and new food assets 1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.vishal.nearbyhungry"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -23,7 +23,7 @@ android {
     defaultConfig {
         applicationId = "com.vishal.nearbyhungry"
         minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
